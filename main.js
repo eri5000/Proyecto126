a2559941c853c879f0704453de263db6fbd22b57
eri5000 <107961194+eri5000@users.noreply.github.com>
2023-06-16 16:45:29 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-canvas(){
+song = "music.mp3";
+song2 ="music2.mp3";
+function canvas(){
     canvas = createCanvas(300,300);
     canvas.center();
+}
+function preload(){
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500);
 }
