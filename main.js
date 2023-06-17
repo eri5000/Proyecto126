4b4824dce83186a8e7c48db47f67cd5c5299e50a
eri5000 <107961194+eri5000@users.noreply.github.com>
2023-06-16 19:46:20 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song = "music.mp3";
 song2 ="music2.mp3";
+leftWritsX = 0;
+leftWristY = 0,
+rightWristX=0;
+rightWristY=0;
 function canvas(){
     canvas = createCanvas(300,300);
     canvas.center();
@@ -14,7 +18,17 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose' , gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
+}
+function gotPoses(results){
+ if(results.length > 0){
+   console.log(results);
+ }
+}
+ function modelLoaded(){
+  console.log('Posenet esta inicializado');
 }
